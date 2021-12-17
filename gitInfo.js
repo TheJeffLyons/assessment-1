6629f98811cdb384703d546cd8c8e679c7a8062d
Jeff Lyons <jeffreylyons@outlook.com>
2021-12-17 14:22:57 -0500

    first commit

---
+++ gitInfo.js
@@ -0,0 +1,79 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+var gitDefinition = `used for code management and functions as a terminal`
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+var gitHubDefinition = `manage git repositories`
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+var gitInitDefinition = `initial repository for push to github`
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+var gitCloneDefinition = `clone or copy repository`
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+var gitStatusDefinition = `see file changes`
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+var gitAddDefinition = `andds new or changed files to be pushed to github`
+
+var gitAddCode = `git add -A`
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+var gitCommitDefinition = `save changes`
+
+var gitCommitCode = `git commit -m "initial commit`
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+var gitPushDefinition = `upload local repository to github`
---
+++ password.js
@@ -0,0 +1,14 @@
+console.log(`Welcome to Jeff's Password Validator`)
+const readline = require(`readline`);
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+
+})
+reader.question("please create a password")
+
+
+var password = 10
+
+if(function(password) === 10
